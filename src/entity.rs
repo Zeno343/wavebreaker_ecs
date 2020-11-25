6edcc88bf3dd0cde2c3ab73fc9aac87b53555aa8
Zeno <zeno@oxydyze.dev>
2020-11-25 14:18:28 -0500

    Implemented EntityManager

--- src/entity.rs
+++ src/entity.rs
@@ -1,3 +1,27 @@
 pub struct Entity {
     pub id: usize,
 }
+
+pub struct EntityManager {
+    current_id: usize,
+}
+
+impl EntityManager {
+    pub fn new() -> Self {
+        Self {
+            current_id: 0
+        }
+    }
+
+    pub fn next_entity(&mut self) -> Entity {
+        let entity = Entity {
+            id: self.current_id
+        };
+
+        self.current_id += 1;
+
+        entity
+    }
+}
+
+
