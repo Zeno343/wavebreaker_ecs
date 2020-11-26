56dba4ff8923893a88e28df2ebb8d92a4edf372e
Zeno <zeno@oxydyze.dev>
2020-11-25 20:34:13 -0500

    Defined World struct to contain entities, components and resources

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,26 @@ pub mod entity;
 #[allow(dead_code)]
 pub mod resource;
 
+use component::ComponentManager;
+use entity::EntityManager;
+use resource::ResourceManager;
+
+pub struct World {
+    pub components: ComponentManager,
+    pub entities: EntityManager,
+    pub resources: ResourceManager,
+}
+
+impl World {
+    pub fn new() -> Self {
+        Self {
+            components: ComponentManager::new(),
+            entities: EntityManager::new(),
+            resources: ResourceManager::new(),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
 
